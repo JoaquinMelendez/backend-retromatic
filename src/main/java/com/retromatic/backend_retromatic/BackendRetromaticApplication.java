0a6d8575f755c3cd465d58d01b19e59532b3c2a5
JoaquinMelendez <jmelendezhenriquez@gmail.com>
2025-11-17 11:01:42 -0300

    chore: inizialite project in Spring Inizializr (JPA, Lombok, H2, S.Web)

---
+++ src/main/java/com/retromatic/backend_retromatic/BackendRetromaticApplication.java
@@ -0,0 +1,13 @@
+package com.retromatic.backend_retromatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRetromaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRetromaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retromatic/backend_retromatic/BackendRetromaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retromatic.backend_retromatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRetromaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
