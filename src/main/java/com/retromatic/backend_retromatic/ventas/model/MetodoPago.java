2e4ca3082e117657175ebf6a32768be642c20a7f
JoaquinMelendez <jmelendezhenriquez@gmail.com>
2025-11-18 13:03:43 -0300

    feat: create tables related to SALE only with their Model.

---
+++ src/main/java/com/retromatic/backend_retromatic/ventas/model/MetodoPago.java
@@ -0,0 +1,32 @@
+package com.retromatic.backend_retromatic.ventas.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetodoPago {
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nombre;
+
+    @OneToMany(mappedBy="estado")
+    private List<Venta> ventas;
+
+}
---
+++ src/main/java/com/retromatic/backend_retromatic/ventas/model/VentaJuego.java
@@ -0,0 +1,38 @@
+package com.retromatic.backend_retromatic.ventas.model;
+
+import com.retromatic.backend_retromatic.juegos.model.Juego;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VentaJuego{
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "juego_id")
+    private Juego juego;
+
+    @ManyToOne
+    @JoinColumn(name = "venta_id")
+    private Venta venta;
+
+}
